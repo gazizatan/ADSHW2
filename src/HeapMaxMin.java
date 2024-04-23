b8261e12efe554f0b94c14f776c05edb2cac7544
gazizatan <153696424+gazizatan@users.noreply.github.com>
2024-04-23 23:53:55 +0500

    HW

--- src/HeapMaxMin.java
+++ src/HeapMaxMin.java
@@ -1,9 +1,40 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class HeapMaxMin {
     public static int heapify(int[] arr) {
-
-
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            maxHeapify(arr, n, i);
+        }
+        return arr[0];
+    }
+    public static void maxHeapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+        if (largest!= i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            maxHeapify(arr, n, largest);
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = heapify(arr);
+        System.out.println(max);
     }
 }
+
--- src/Recursion.java
+++ src/Recursion.java
@@ -1,14 +1,43 @@
-//I made this on Python, so check pls README
+//I made in Python
+/*
+class Recursion:
+    def __init__(self):
+        self.items = []
 
-/*import java.util.ArrayList;
+    def push(self, item):
+        self.items.append(item)
 
-public class Recursion {
-    public static void main(String[] args) {
+    def pop(self):
+        return self.items.pop()
 
-    }
+    def is_empty(self):
+        return len(self.items) == 0
 
-    public static void reverseList(ArrayList<Integer> list, int start, int end) {
+    def peek(self):
+        return self.items[-1]
 
-    }
-}
-*/
+    def reverse(self):
+        if not self.is_empty():
+            top = self.pop()
+            self.reverse()
+            self.insert_at_bottom(top)
+
+    def insert_at_bottom(self, item):
+        if self.is_empty():
+            self.push(item)
+        else:
+            top = self.pop()
+            self.insert_at_bottom(item)
+            self.push(top)
+
+input_str = input()
+
+input_list = list(map(int, input_str.split()))
+
+stack = Recursion()
+list(map(stack.push, input_list))
+
+stack.reverse()
+
+print(stack.items)
+ */
